67b4ed2c1d32f74e060c42e8aa70e9c44aa5b594
Orlo Montesdeoca <76452632+itsOrlo@users.noreply.github.com>
2024-02-29 13:41:57 +0000

    corrección errores por consola

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@
         }
     });
 
-
     // Smooth scrolling on the navbar links
     $(".navbar-nav a").on('click', function (event) {
         if (this.hash !== "") {
@@ -27,7 +26,6 @@
         }
     });
 
-
     // Typed Initiate
     if ($('.typed-text-output').length == 1) {
         var typed_strings = $('.typed-text').text();
@@ -39,7 +37,6 @@
             loop: true
         });
     }
-
 
     // Modal Video
     $(document).ready(function () {
@@ -58,7 +55,6 @@
         })
     });
 
-
     // Scroll to Bottom
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -68,7 +64,6 @@
         }
     });
 
-
     // Skills
     $('.skill').waypoint(function () {
         $('.progress .progress-bar').each(function () {
@@ -76,24 +71,21 @@
         });
     }, { offset: '80%' });
 
-
-
     $(document).ready(function () {
         // Dejar seleccionado el elemento "Desarrollo" por defecto
         $('#portfolio-flters li[data-filter=".dev"]').addClass('active');
-        
-        var portfolioIsotope = $('.portfolio-container').isotope({
-          itemSelector: '.portfolio-item',
-          layoutMode: 'fitRows'
-        });
-      
-        $('#portfolio-flters li').on('click', function () {
-          $('#portfolio-flters li').removeClass('active');
-          $(this).addClass('active');
-          portfolioIsotope.isotope({ filter: $(this).data('filter') });
-        });
-      });
 
+        var portfolioIsotope = $('.portfolio-container').isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
+
+        $('#portfolio-flters li').on('click', function () {
+            $('#portfolio-flters li').removeClass('active');
+            $(this).addClass('active');
+            portfolioIsotope.isotope({ filter: $(this).data('filter') });
+        });
+    });
 
     // Back to top button
     $(window).scroll(function () {
@@ -109,8 +101,6 @@
         $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
-    
-
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
@@ -121,66 +111,66 @@
         items: 1
     });
 
-})(jQuery);
+    // Obtener referencia al botón y al modal
+    const botonEnviar = document.getElementById('boton-enviar');
+    const modalMensajeEnviado = document.getElementById('modal-mensaje-enviado');
 
-// Obtener referencia al botón y al modal
-const botonEnviar = document.getElementById('boton-enviar');
-const modalMensajeEnviado = document.getElementById('modal-mensaje-enviado');
-
-// Función para mostrar el modal de "mensaje enviado"
-function mostrarModalMensajeEnviado() {
-    modalMensajeEnviado.style.display = 'block';
-}
-
-// Función para verificar el campo de correo electrónico
-function verificarCorreo() {
-    const campoCorreo = document.getElementById('correo');
-    const correo = campoCorreo.value.trim();
-
-    if (correo === '') {
-        alert('Por favor, ingresa un correo electrónico.');
-        return false;
+    // Función para mostrar el modal de "mensaje enviado"
+    function mostrarModalMensajeEnviado() {
+        modalMensajeEnviado.style.display = 'block';
     }
 
-    // Verificar el formato del correo electrónico utilizando una expresión regular simple
-    const correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!correoRegex.test(correo)) {
-        mostrarError();
-        return false;
+    // Función para verificar el campo de correo electrónico
+    function verificarCorreo() {
+        const campoCorreo = document.getElementById('correo');
+        const correo = campoCorreo.value.trim();
+
+        if (correo === '') {
+            alert('Por favor, ingresa un correo electrónico.');
+            return false;
+        }
+
+        // Verificar el formato del correo electrónico utilizando una expresión regular simple
+        const correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!correoRegex.test(correo)) {
+            mostrarError();
+            return false;
+        }
+
+        return true;
     }
 
-    return true;
-}
+    // Asignar la función de verificación al evento click del botón utilizando jQuery
+    $('#boton-enviar').on('click', function (event) {
+        event.preventDefault(); // Evitar el envío del formulario por defecto
 
-// Asignar la función de verificación al evento click del botón
-botonEnviar.addEventListener('click', function (event) {
-    event.preventDefault(); // Evitar el envío del formulario por defecto
-
-    if (verificarCorreo()) {
-        pregunta();
-    } else{
-        mostrarError();
-    }
-});
-
-// Sweet Alert
-function pregunta(){  
-
-    swal({
-      title: "¡Enviado! :D",
-      text: "Pronto me pondré en contacto contigo",
-      type: "success",
-      timer: 500,
-      showConfirmButton: false
-    }, function(){
-          window.location.href = "http://127.0.0.1:5500/index.html";
+        if (verificarCorreo()) {
+            pregunta();
+        } else {
+            mostrarError();
+        }
     });
-         } 
-function mostrarError() {
-            Swal.fire({
-              icon: 'error',
-              title: 'Error',
-              text: 'Ocurrió un error en el proceso.',
-              confirmButtonText: 'OK'
-            });
-          }
+
+    // Sweet Alert
+    function pregunta() {
+        swal({
+            title: "¡Enviado! :D",
+            text: "Pronto me pondré en contacto contigo",
+            type: "success",
+            timer: 500,
+            showConfirmButton: false
+        }, function () {
+            window.location.href = "https://itsorlo.me/";
+        });
+    }
+
+    function mostrarError() {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Ocurrió un error en el proceso.',
+            confirmButtonText: 'OK'
+        });
+    }
+
+})(jQuery);
